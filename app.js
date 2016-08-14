d77fdbc6f48bb3a9db0fda13148643f751eed676
Chris Hurlburt <churlburt132@g.rwu.edu>
2016-08-14 01:12:52 -0400

    real time sentiment analysis with index from -1 to 1

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const env = require('dotenv').config();
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const sentiment = require('sentiment')
+const twitter = require('./lib/twitter')
+const analyzer = require('./lib/analyzer')
+const io = require('socket.io')()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const routes = require('./routes/index');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+const app = express();
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +22,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-// app.use('/users', users);
+
+// Setup socket server
+io.listen(3001);
+
+// Set keyword
+twitter.track('olympics')
+
+// Real-time sentiment analysis (-1 to 1)
+twitter.on('tweet', (tweet) => {
+  let score = analyzer.tweet(tweet.text)
+  if (score) io.emit('score', score)
+})
+
+twitter.on('error', function (err) {
+  console.log('error')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
